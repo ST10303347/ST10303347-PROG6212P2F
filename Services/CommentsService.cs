35070e317a852e479ee71357955bd5c6957906c6
ZAYYYY <st10303347@vcconnect.edu.za>
2024-10-18 17:46:35 +0200

    I now have added the comments service and comments interface follwoing the pattern of claims

---
+++ Services/CommentsService.cs
@@ -0,0 +1,21 @@
+﻿using ST10303347_PROG6212P2F.Models;
+using ST10303347_PROG6212P2F.Services;
+
+namespace ST10303347_PROG6212P2F.Data.Services
+{
+    public class CommentsService : ICommentsService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Comment comment)
+        {
+            _context.comments.Add(comment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Services/ICommentsService.cs
@@ -0,0 +1,9 @@
+﻿using ST10303347_PROG6212P2F.Models;
+
+namespace ST10303347_PROG6212P2F.Services
+{
+    public interface ICommentsService
+    {
+        Task Add(Comment comment);
+    }
+}
