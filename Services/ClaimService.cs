5d0f3fadcab1b181279d4e460f25415a4eb67ee7
ZAYYYY <st10303347@vcconnect.edu.za>
2024-10-18 17:40:39 +0200

    Claim service and claim now have their operations set

--- Services/ClaimService.cs
+++ Services/ClaimService.cs
@@ -1,6 +1,47 @@
-﻿namespace ST10303347_PROG6212P2F.Services
+﻿using Microsoft.EntityFrameworkCore;
+using ST10303347_PROG6212P2F.Models;
+using ST10303347_PROG6212P2F.Services;
+
+namespace ST10303347_PROG6212P2F.Data.Services
 {
-    public class ClaimService
+    public class ClaimService : IClaimService
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClaimService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Claim claim)
+        {
+            _context.claims.Add(claim);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<Claim> GetAll()
+        {
+            var claims = _context.claims
+                .Include(c => c.User)
+                .Include(c => c.Comments)
+                .Include(c => c.SupportingDocuments);
+            return claims;
+        }
+
+        public async Task<Claim> GetById(int? id)
+        {
+            var claim = await _context.claims
+                .Include(c => c.User)
+                .Include(c => c.Comments)
+                .Include(c => c.SupportingDocuments)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return claim;
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
     }
 }
+
--- Services/IClaimService.cs
+++ Services/IClaimService.cs
@@ -2,7 +2,7 @@
 
 namespace ST10303347_PROG6212P2F.Services
 {
-    public interface IClaimService
+    public interface IClaimService 
     {
         IQueryable<Claim> GetAll();
         Task Add(Claim claim);
